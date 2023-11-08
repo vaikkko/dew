0f7d1c5d8bc90a452c34fe26d494610d257642c4
vaikkko <victor3blm@gmail.com>
2023-11-08 16:02:09 +0000

    new update

--- ut4/DWEC04 - Tarea Evaluable/script.js
+++ ut4/DWEC04 - Tarea Evaluable/script.js
@@ -1,8 +1,18 @@
-function edificio(calle,numero,codigo,){
+class edificio{
+        constructor(calle,numero,cp){
 
-    this.calle = calle;
-    this.numero = numero;
-    this.codigo = codigo;
-    this.plantas = 0;
-    
+        this.calle = calle;
+        this.numero = numero;
+        this.cp = cp;
+        this.plantas = 0 ;
+        this.puertas =  ;
+
+        function agregarPlantasYPuertas(numPlantas, puertas){
+            this.plantas = numPlantas;
+            this.puertas = puertas;
+        }
+        function modificarNumero(numero){
+            this.numero = numero;
+        }
+    }
 }
