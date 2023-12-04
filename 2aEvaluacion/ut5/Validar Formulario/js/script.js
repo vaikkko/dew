13d04de4774a6e0ed7671ea96b3bd3e48e948415
Victor <victor3blm@gmail.com>
2023-12-04 18:57:31 +0000

    new files

--- 2aEvaluacion/ut5/Validar Formulario/js/script.js
+++ 2aEvaluacion/ut5/Validar Formulario/js/script.js
@@ -5,12 +5,19 @@ const edad = document.getElementById("edad");
 const nif = document.getElementById("nif");
 const email = document.getElementById("email");
 const provincia = document.getElementById("provincia");
+const fecha = document.getElementById("fecha");
+const telefono = document.getElementById("telefono");
+const hora = document.getElementById("hora");
 
 nombre.addEventListener("blur", checkName);
 apellidos.addEventListener("blur", checkApellidos);
 edad.addEventListener("blur", checkEdad);
 nif.addEventListener("blur", checkNif);
 email.addEventListener("blur", checkEmail);
+provincia.addEventListener("blur", checkProvincia);
+fecha.addEventListener("blur", checkFecha);
+telefono.addEventListener("blur", checkTelefono);
+hora.addEventListener("blur", checkHora);
 
 function checkName(e) {
   const value = e.target.value;
@@ -27,7 +34,7 @@ function checkApellidos(e) {
     console.log("No puedes dejar vacio el campo Apellidos");
   } else {
     value.toUpperCase();
-  }
+  }   
 }
 
 function checkEdad(e) {
@@ -48,6 +55,7 @@ function checkNif(e) {
     console.log("El número NIF es obligatorio");
   } else if (!pat.test(value)) {
     console.log("No es un número NIF válido, revisalo.");
+    document.getElementById("errores").innerHTML = "No es un número NIF válido, revisalo.";
   } else {
     console.log("Número Correcto");
   }
@@ -60,7 +68,47 @@ function checkEmail(e) {
     console.log("El campo Email es obligatorio");
   } else if (!pat.test(value)) {
     console.log("Escribe un Email válido");
-  }else{
+  } else {
     console.log("Email correcto");
+  }
+}
+
+function checkProvincia(e) {}
+
+function checkFecha(e) {
+  const value = e.target.value;
+  const pat =
+    /^(?:(?:(?:0?[1-9]|1\d|2[0-8])[/](?:0?[1-9]|1[0-2])|(?:29|30)[/](?:0?[13-9]|1[0-2])|31[/](?:0?[13578]|1[02]))[/](?:0{2,3}[1-9]|0{1,2}[1-9]\d|0?[1-9]\d{2}|[1-9]\d{3})|29[/]0?2[/](?:\d{1,2}(?:0[48]|[2468][048]|[13579][26])|(?:0?[48]|[13579][26]|[2468][048])00))$/;
+
+  if (value.length === 0) {
+    console.log("El campo Fecha es obligatorio");
+  } else if (!pat.test(value)) {
+    console.log("Introduce una fecha válida, dd/mm/aaaa.");
+  } else {
+    console.log("Fecha correcta");
+  }
+}
+
+function checkTelefono(e) {
+  const value = e.target.value;
+  const pat = /^[09][0-9]{1,7}$/;
+  if (value.length === 0) {
+    console.log("No puedes dejar el campo del Teléfono en blanco");
+  } else if (!pat.test(value)) {
+    console.log("Escribe un número válido");
+  } else {
+    console.log("Número correcto");
+  }
+}
+
+function checkHora(e) {
+  const value = e.target.value;
+  const pat = /^([01]?[0-9]|2[0-3]):[0-5][0-9]$/;
+  if (value.length === 0) {
+    console.log("No puedes dejar el campo de la Hora en blanco");
+  } else if (!pat.test(value)) {
+    console.log("Escribe una hora válida, hh:mm.");
+  } else {
+    console.log("Hora correcta");
   }
 }
