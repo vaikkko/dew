e3c07f2fc798956f1445e17aa65cde51639022b2
Victor <victor3blm@gmail.com>
2023-12-04 20:13:15 +0000

    new functions

--- 2aEvaluacion/ut5/Validar Formulario/js/script.js
+++ 2aEvaluacion/ut5/Validar Formulario/js/script.js
@@ -19,32 +19,45 @@ fecha.addEventListener("blur", checkFecha);
 telefono.addEventListener("blur", checkTelefono);
 hora.addEventListener("blur", checkHora);
 
+
+
+function enviar(e){
+
+  
+}
+
 function checkName(e) {
   const value = e.target.value;
   if (value.length === 0) {
-    console.log("No puedes dejar vacio el campo Nombre");
+    document.getElementById("errores").innerHTML =
+      "No puedes dejar el Nombre en blanco.";
   } else {
-    value.toUpperCase();
+    e.target.value = value.toUpperCase();
+    document.getElementById("errores").innerHTML = "";
   }
 }
 
 function checkApellidos(e) {
   const value = e.target.value;
   if (value.length === 0) {
-    console.log("No puedes dejar vacio el campo Apellidos");
+    document.getElementById("errores").innerHTML =
+      "No puedes dejar los Apellidos en blanco.";
   } else {
-    value.toUpperCase();
-  }   
+    e.target.value = value.toUpperCase();
+    document.getElementById("errores").innerHTML = "";
+  }
 }
 
 function checkEdad(e) {
   const value = e.target.value;
   if (value.length === 0) {
-    console.log("La edad es un campo obligatorio");
+    document.getElementById("errores").innerHTML =
+      "La edad es un campo obligatorio";
   } else if (isNaN(value)) {
-    console.log("La edad solo puede ser un valor numérico");
-  } else if (parseInt(value) > 105) {
-    console.log("Escribe una edad válida");
+    document.getElementById("errores").innerHTML =
+      "La edad solo puede ser un valor numérico";
+  } else if (parseInt(value) > 105 || parseInt(value)<0) {
+    document.getElementById("errores").innerHTML = "Escribe una edad válida";
   }
 }
 
@@ -53,11 +66,13 @@ function checkNif(e) {
   const pat = /(^([0-9]{8,8}\-[A-Z])|^)$/;
   if (value.length === 0) {
     console.log("El número NIF es obligatorio");
+    document.getElementById("errores").innerHTML =
+      "El campo NIF es obligatorio";
   } else if (!pat.test(value)) {
-    console.log("No es un número NIF válido, revisalo.");
-    document.getElementById("errores").innerHTML = "No es un número NIF válido, revisalo.";
+    document.getElementById("errores").innerHTML =
+      "No es un número NIF válido, XXXXXXXX-L.";
   } else {
-    console.log("Número Correcto");
+    document.getElementById("errores").innerHTML = "";
   }
 }
 
@@ -65,15 +80,24 @@ function checkEmail(e) {
   const value = e.target.value;
   const pat = /^[^@\s]+@[^@\.\s]+(\.[^@\.\s]+)+$/;
   if (value.length === 0) {
-    console.log("El campo Email es obligatorio");
+    document.getElementById("errores").innerHTML =
+      "El campo Email es obligatorio";
   } else if (!pat.test(value)) {
-    console.log("Escribe un Email válido");
+    document.getElementById("errores").innerHTML = "Escribe un Emaill válido";
   } else {
-    console.log("Email correcto");
+    document.getElementById("errores").innerHTML = "";
   }
 }
 
-function checkProvincia(e) {}
+function checkProvincia(e) {
+  const value = e.target.value[0];
+  if (value === "0") {
+    document.getElementById("errores").innerHTML =
+      "Seleccionar una Provincia es obligatorio.";
+  } else {
+    document.getElementById("errores").innerHTML = "";
+  }
+}
 
 function checkFecha(e) {
   const value = e.target.value;
@@ -81,23 +105,26 @@ function checkFecha(e) {
     /^(?:(?:(?:0?[1-9]|1\d|2[0-8])[/](?:0?[1-9]|1[0-2])|(?:29|30)[/](?:0?[13-9]|1[0-2])|31[/](?:0?[13578]|1[02]))[/](?:0{2,3}[1-9]|0{1,2}[1-9]\d|0?[1-9]\d{2}|[1-9]\d{3})|29[/]0?2[/](?:\d{1,2}(?:0[48]|[2468][048]|[13579][26])|(?:0?[48]|[13579][26]|[2468][048])00))$/;
 
   if (value.length === 0) {
-    console.log("El campo Fecha es obligatorio");
+    document.getElementById("errores").innerHTML =
+      "El campo Fecha es obligatorio";
   } else if (!pat.test(value)) {
-    console.log("Introduce una fecha válida, dd/mm/aaaa.");
+    document.getElementById("errores").innerHTML =
+      "Introdce una fecha válida, dd/mm/aaaa.";
   } else {
-    console.log("Fecha correcta");
+    document.getElementById("errores").innerHTML = "";
   }
 }
 
 function checkTelefono(e) {
   const value = e.target.value;
-  const pat = /^[09][0-9]{1,7}$/;
+  const pat = /^([6789][0-9]{8,13})$/;
   if (value.length === 0) {
-    console.log("No puedes dejar el campo del Teléfono en blanco");
+    document.getElementById("errores").innerHTML =
+      "No puedes dejar el campo Teléfono en blanco";
   } else if (!pat.test(value)) {
-    console.log("Escribe un número válido");
+    document.getElementById("errores").innerHTML = "Escribe un teléfono válido";
   } else {
-    console.log("Número correcto");
+    document.getElementById("errores").innerHTML = "";
   }
 }
 
@@ -105,10 +132,12 @@ function checkHora(e) {
   const value = e.target.value;
   const pat = /^([01]?[0-9]|2[0-3]):[0-5][0-9]$/;
   if (value.length === 0) {
-    console.log("No puedes dejar el campo de la Hora en blanco");
+    document.getElementById("errores").innerHTML =
+      "No puedes dejar el campo de la Hora en blanco";
   } else if (!pat.test(value)) {
-    console.log("Escribe una hora válida, hh:mm.");
+    document.getElementById("errores").innerHTML =
+      "Escribe una hora válida, hh:mm.";
   } else {
-    console.log("Hora correcta");
+    document.getElementById("errores").innerHTML = "";
   }
 }
